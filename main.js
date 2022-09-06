16efe9a162f485b91ca8339555e7415ada79c763
d4nQw3rty <danlinmoy@gmail.com>
2022-09-06 18:13:36 -0400

    Update main.js
    -Add close function to hide menu

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function openMenu() {
 }
 
 btn.addEventListener('click', openMenu);
+
+const closeBtn = document.querySelector('.x-btn');
+
+function closeMenu() {
+    document.querySelector('.menu-overlay').style.display = 'none';
+}
+
+closeBtn.addEventListener('click', closeMenu)
+
