0f406089708229d7dc12520fc61ba94f7fdaefcd
PedroH-Neves <PedroHDN710@gmail.com>
2022-09-12 18:24:58 -0400

    Add dynamic project section

--- main.js
+++ main.js
@@ -17,3 +17,84 @@ closeBtn.addEventListener('click', closeMenu);
 const closeBtn2 = document.querySelector('.mobile-menu');
 
 closeBtn2.addEventListener('click', closeMenu);
+
+const sectionCards = document.getElementById('portfolio');
+
+const workArr = [
+  {
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: "./assets/Projects/Img\ Placeholder\ 2.svg",
+    technologies: ["rust", "elixir", "lua"],
+    linkToLiveVersion: "https://pedroh-neves.github.io/PedroNeves.github.io/",
+    linkToSource: "https://github.com/PedroH-Neves/PedroNeves.github.io",
+  },
+
+  {
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: "./assets/Projects/Img\ Placeholder\ 2.svg",
+    technologies: ["rust", "elixir", "lua"],
+    linkToLiveVersion: "https://pedroh-neves.github.io/PedroNeves.github.io/",
+    linkToSource: "https://github.com/PedroH-Neves/PedroNeves.github.io",
+  },
+
+  {
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: "./assets/Projects/Img\ Placeholder\ 2.svg",
+    technologies: ["rust", "elixir", "lua"],
+    linkToLiveVersion: "https://pedroh-neves.github.io/PedroNeves.github.io/",
+    linkToSource: "https://github.com/PedroH-Neves/PedroNeves.github.io",
+  },
+
+  {
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: "./assets/Projects/Img\ Placeholder\ 2.svg",
+    technologies: ["rust", "elixir", "lua"],
+    linkToLiveVersion: "https://pedroh-neves.github.io/PedroNeves.github.io/",
+    linkToSource: "https://github.com/PedroH-Neves/PedroNeves.github.io",
+  },
+
+  {
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: "./assets/Projects/Img\ Placeholder\ 2.svg",
+    technologies: ["rust", "elixir", "lua"],
+    linkToLiveVersion: "https://pedroh-neves.github.io/PedroNeves.github.io/",
+    linkToSource: "https://github.com/PedroH-Neves/PedroNeves.github.io",
+  },
+
+  {
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: "./assets/Projects/Img\ Placeholder\ 2.svg",
+    technologies: ["rust", "elixir", "lua"],
+    linkToLiveVersion: "https://pedroh-neves.github.io/PedroNeves.github.io/",
+    linkToSource: "https://github.com/PedroH-Neves/PedroNeves.github.io",
+  },
+
+];
+
+workArr.forEach(item => {
+  console.log(item)
+  sectionCards.innerHTML += ` 
+  <div class="project-box-two project-box-two-hover">
+          <h3 id="projectTwo" class="project-title-two">
+            ${item.name}
+          </h3>
+          <p class="project-text-two">
+            ${item.description}
+          </p>
+          <ul class="project-list-tags-two">
+          ${item.technologies.map(tag => {
+            return `<li class="project-list-item-two"><span class="tag">${tag}</span></li>`;
+          }).join('')}
+          </ul>
+          <a href="#"
+            class="project-link-two primary-color no-underscore button-hover button-pressed button-disabled">See
+            Project</a>
+        </div>
+  `;
+});
